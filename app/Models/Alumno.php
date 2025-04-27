6bcc3b4099c318df43115048f9c5a6cb507a55a8
CristianIsaacAguilarGuzman <cisaac.aguilar@alumnos.udg.mx>
2025-04-27 16:52:06 -0600

    Inicio de la Actividad de Relación muchos a muchos

--- app/Models/Alumno.php
+++ app/Models/Alumno.php
@@ -9,6 +9,11 @@ class Alumno extends Model
 {
     use HasFactory;
 
+        public function secciones()
+        {
+            return $this->belongsToMany(Seccion::class);
+        }
+
     // Definir los campos que se pueden asignar masivamente
     protected $fillable = ['nombre', 'correo', 'fecha_nacimiento', 'ciudad'];
 }
